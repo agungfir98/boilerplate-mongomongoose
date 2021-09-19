35887c1da43cf28b758e126f832e9b1f3131a991
Agung Firmansyah <agungfir98@gmail.com>
2021-09-19 14:31:30 +0700

    creating a model

--- myApp.js
+++ myApp.js
@@ -1,11 +1,23 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const {Schema, model} = mongoose;
 mongoose.connect(process.env.MONGO_URI)
 
+// Creating a model
+const PersonSchema = new Schema({
+  name: {type: String, required: true},
+  age: {type: Number},
+  FavoriteFoods: [String]
+});
 
+let Person = model('Person', PersonSchema);
 
-let Person;
+// model example
+const agung = new Person({
+  name: 'Agung',
+  age: 23,
+  FavoriteFoods: ['Mie', 'Mendoan']
+})
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
